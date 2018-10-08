24b02fadd003e4bcd1b37c6657173e7b366fffcd
wuxiaolin <wuxiaolin@gaosiedu.com>
2018-10-08 18:57:59 +0800

    first commit.

---
+++ VUE-SSR-TECH/src/app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <span id="test">
+      {{text}}
+    </span>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        text: 'abc'
+      }
+    }
+  }
+</script>
+
+<style>
+#test{
+  color: red;
+}
+</style>
+
