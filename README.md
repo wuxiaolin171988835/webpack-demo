ee4cf62619b720493648c980877359b376912be3
wuxiaolin <wuxiaolin@gaosiedu.com>
2018-10-12 18:41:20 +0800

    Add babel.

---
+++ README.md
@@ -0,0 +1,15 @@
+webpack基础应用：
+
+项目启动
+npm run dev
+
+环境：
+development、production
+
+
+解析：
+htmlWebpackPlugin：生成html文件
+loader适配：css-loader、url-loader...
+config.devServer: 开发环境启动一个服务
+  hot: true 局部刷新，需要HotModuleReplacementPlugin
+
---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+const isDev = process.env.NODE_ENV === 'development'
+const config = {
+  target: 'web',
+  entry: path.join(__dirname, 'src/index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist')
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: isDev ? '"development"' : '"production"'
+      }
+    }),
+    //生成html
+    new htmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },{
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          'less-loader'
+        ]
+      },
+      {
+        test: /\.(jpg|jpeg|svg|gif|png)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024,
+              name:'[name]-aaa.[ext]'
+            }
+          }
+        ]
+      }
+    
+    ]
+  }
+}
+
+if(isDev){
+  config.devtool = '#cheap-module-eval-source-map'
+  //开发环境本地启动一个服务
+  config.devServer = {
+    port: 8024,
+    host: '0.0.0.0',
+    overlay: {
+      errors: true
+    },
+    //组件更新，不需要刷新页面，需要HotModuleReplacementPlugin
+    hot: true
+  }
+  config.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  )
+}
+module.exports = config
